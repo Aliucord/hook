26de91ff4541a0533987a23be5f78b9803666fe6
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-07-08 14:19:28 -0700

    fix: test activity

---
+++ example/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    compileSdk = 36
+    namespace = "com.aliucord.hook.example"
+
+    defaultConfig {
+        applicationId = "com.aliucord.hook.example"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+}
